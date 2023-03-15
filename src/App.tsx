ae6380a29ee9bbacf96e8de2f61f15ace9284115
HyungwonJin <hwjin92@gmail.com>
2023-03-15 12:07:57 +0900

    chore: IssueCount component will not re-render if the pageNumber value remains the same

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import {IssueCountType} from "./types/issueCount";
 function App() {
   const [inputValue, setInputValue] = useState({org: 'Facebook', repo: 'react', pageNumber: "1"});
   const {data: lists, isLoading: isListLoading} = useQuery<ListsType[]>(['issues', inputValue],() => fetchIssueList(inputValue))
-  const {data: count, isLoading: isCountLoading} = useQuery<IssueCountType>(['issueCount', inputValue], () => fetchCount({org:inputValue.org, repo: inputValue.repo}))
+  const {data: count, isLoading: isCountLoading} = useQuery<IssueCountType>(['issueCount', inputValue.org, inputValue.repo], () => fetchCount({org:inputValue.org, repo: inputValue.repo}))
   const countCheck = count ? <IssueCount count={count} /> : <span>Something wrong...</span>
   return (
     <div className="p-4 max-w-[820px] min-w-[820px] my-0 mx-auto">
