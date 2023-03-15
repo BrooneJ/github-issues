574faf54aa427a2fba1470fe3908dd01dba1a3b5
HyungwonJin <hwjin92@gmail.com>
2023-03-15 12:37:46 +0900

    feat: Added error handling code

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,42 @@ import {IssueCountType} from "./types/issueCount";
 
 function App() {
   const [inputValue, setInputValue] = useState({org: 'Facebook', repo: 'react', pageNumber: "1"});
-  const {data: lists, isLoading: isListLoading} = useQuery<ListsType[]>(['issues', inputValue],() => fetchIssueList(inputValue))
-  const {data: count, isLoading: isCountLoading} = useQuery<IssueCountType>(['issueCount', inputValue.org, inputValue.repo], () => fetchCount({org:inputValue.org, repo: inputValue.repo}))
-  const countCheck = count ? <IssueCount count={count} /> : <span>Something wrong...</span>
+  const {
+    data: lists,
+    isLoading: isListLoading,
+    error: listError
+  } = useQuery<ListsType[]>(['issues', inputValue], () => fetchIssueList(inputValue))
+
+  const {
+    data: count,
+    isLoading: isCountLoading,
+    error: countError
+  } = useQuery<IssueCountType>(['issueCount', inputValue.org, inputValue.repo], () => fetchCount({
+    org: inputValue.org,
+    repo: inputValue.repo
+  }))
+
+  const countCheck = count ? <IssueCount count={count}/> : <span>Something wrong...</span>
+
+  const isError = listError || countError
+  const isLoading = isListLoading || isCountLoading
+  const errorMessage = (listError instanceof Error ? listError.message : "An error occurred.") || (countError instanceof Error ? countError.message : "An error occurred.");
+
   return (
     <div className="p-4 max-w-[820px] min-w-[820px] my-0 mx-auto">
       <SearchBar setInputValue={setInputValue}/>
-      {isCountLoading ? <span>Loading...</span> : countCheck}
-      {isListLoading ? <span>Loading...</span> : <>
-        {lists?.map(list => (<IssueLists list={list}/>))}
-      </>}
+      {isLoading ? <span>Loading...</span> :
+        <>
+          {isError ? <div className="text-red-500 text-3xl mt-10">Error occurred: {errorMessage}</div> :
+            <>
+              {countCheck}
+              {isListLoading ? <span>Loading...</span> : <>
+                {lists?.map(list => (<IssueLists list={list}/>))}
+              </>}
+            </>
+          }
+        </>
+      }
     </div>
   );
 }
