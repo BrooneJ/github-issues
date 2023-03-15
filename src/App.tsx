802ae94ec2a5568e257b02864a1800d4d6b12bc9
HyungwonJin <hwjin92@gmail.com>
2023-03-15 10:27:35 +0900

    feat: Create IssueCount Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,31 @@ import React from 'react';
 import './App.css';
 import SearchBar from "./components/SearchBar";
 import {useQuery} from "react-query";
-import {fetchIssueList} from "./api/api";
+import {fetchCount, fetchIssueList} from "./api/api";
 import IssueLists from "./components/IssueLists";
 import {ListsType} from "./types/issueList";
+import IssueCount from "./components/IssueCount";
+
+export type IssueCountType = {
+  html_url: string
+  name: string
+  open_issues_count: number
+  owner: {
+    login: string
+    html_url: string
+  }
+}
 
 function App() {
-  const {data: lists, isLoading} = useQuery<ListsType[]>('issues', fetchIssueList)
-  console.log(lists)
+  const {data: lists, isLoading: isListLoading} = useQuery<ListsType[]>('issues', fetchIssueList)
+  const {data: count, isLoading: isCountLoading} = useQuery<IssueCountType>('issueCount', fetchCount)
+  const countCheck = count ? <IssueCount count={count} /> : <span>Something wrong...</span>
   return (
     <div className="p-4 max-w-[820px] min-w-[820px] my-0 mx-auto">
       <SearchBar/>
-      {isLoading ? <span>isLoading</span> : <>
-        {lists?.map(list => (<IssueLists list={list} />))}
+      {isCountLoading ? <span>Loading...</span> : countCheck}
+      {isListLoading ? <span>Loading...</span> : <>
+        {lists?.map(list => (<IssueLists list={list}/>))}
       </>}
     </div>
   );
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,13 @@
-export const fetchIssueList = () => {
-  return fetch(`https://api.github.com/repos/Facebook/react/issues?page=1&per_page=25`).then(response => response.json())
+import axios from "axios";
+import {ListsType} from "../types/issueList";
+
+export const fetchIssueList = async () => {
+  const response = await axios.get<ListsType[]>(`https://api.github.com/repos/Facebook/react/issues?page=1&per_page=25`)
+  return response.data
+  // return fetch(`https://api.github.com/repos/Facebook/react/issues?page=1&per_page=25`).then(response => response.json())
+}
+
+export const fetchCount = async () => {
+  const response = await axios.get(`https://api.github.com/repos/Facebook/react`)
+  return response.data
 }
